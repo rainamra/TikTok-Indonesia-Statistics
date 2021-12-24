0fe0a688327af01ae346420458851ab34bbc5e19
christynatalia <57931555+christynatalia@users.noreply.github.com>
2021-12-24 18:02:29 +0700

    christy is struggling. merry x'mas >.<

--- combine_all_things.py
+++ combine_all_things.py
@@ -1,5 +1,13 @@
+import os
+import glob
 import pandas as pd
 
-df_beauty = pd.read_csv('./CLEAN_DATASETS/cleaning_beauty_datasets.csv')
-df_beauty['category'] = 1
-print(df_beauty.head())
+os.chdir("/Users/christynatalia/Desktop/project/tiktok-api/CLEAN_DATASETS")
+
+all_filenames = [glob.glob("*.csv")]
+print(f"These are all of the filenames ending in videos.csv {all_filenames}.")
+
+
+#df_beauty = pd.read_csv('./CLEAN_DATASETS/cleaning_beauty_datasets.csv')
+#df_beauty['category'] = 1
+#print(df_beauty.head())
