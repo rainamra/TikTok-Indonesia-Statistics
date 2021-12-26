a4abbde9ea99ee56a03b84b33aa733c5b28f01d4
christynatalia <57931555+christynatalia@users.noreply.github.com>
2021-12-26 16:22:01 +0700

    I made the category of the content and then combine them all together

--- combine_all_things.py
+++ combine_all_things.py
@@ -4,10 +4,15 @@ import pandas as pd
 
 os.chdir("/Users/christynatalia/Desktop/project/tiktok-api/CLEAN_DATASETS")
 
-all_filenames = [glob.glob("*.csv")]
+all_filenames = glob.glob("*.csv")
 print(f"These are all of the filenames ending in videos.csv {all_filenames}.")
 
+appended_data = []
+for i in range(6):
+    df_file = pd.read_csv(all_filenames[i])
+    df_file['Category'] = i+1
+    appended_data.append(df_file)
+appended_data = pd.concat(appended_data, ignore_index=True)
 
-#df_beauty = pd.read_csv('./CLEAN_DATASETS/cleaning_beauty_datasets.csv')
-#df_beauty['category'] = 1
-#print(df_beauty.head())
+
+appended_data.to_csv("combined_all_things2.csv", index=False)
