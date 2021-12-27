5949d29aeb3602076116bc436de89f909b08c91d
rainamra <raina_mira@yahoo.com>
2021-12-27 18:10:15 +0700

    add columns to be drop

--- clean_combine.py
+++ clean_combine.py
@@ -7,7 +7,7 @@ combined_profile = pd.read_csv("DATASETS/beauty_complete_dataset.csv")
 combined_profile.info()
 
 #drop the columns for index 
-combined_profile.drop(['video_time','video_link', "Unnamed: 0", "Unnamed: 0_x", "Unnamed: 0_y"], axis =1, inplace=True)
+combined_profile.drop(['video_time','video_link', "Unnamed: 0", "Unnamed: 0_x", "Unnamed: 0_y", "user_bio", "user_total_hearts", "user_id", "video_id", "music_id"], axis =1, inplace=True)
 combined_profile.to_csv("cleaning_beauty_datasets.csv", index=False)
 
 
--- combine_multiple.py
+++ combine_multiple.py
@@ -21,10 +21,3 @@ os.chdir('/Users/rainamra/Documents/DataScience/Final_project/TECH')
 combined_csv_profile.to_csv('tech_combined_profile.csv')
 combined_csv_videos.to_csv('tech_combined_videos.csv')
 
-# all_combined = combined_csv_profile.merge(combined_csv_videos,left_on="user_name", right_on="user_name")
-# all_combined = pd.merge(combined_csv_videos, combined_csv_profile, on="user_name", how="outer")
-# drop_duplicates = all_combined.drop_duplicates()
-
-# os.chdir('/Users/rainamra/Documents/DataScience/Final_project/DATASETS')
-# drop_duplicates.to_csv('tech_no_duplicates_dataset.csv')
-
